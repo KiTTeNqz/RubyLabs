b9e6f3516f7dee8740db01eb533b88defd177396
papaSquid <mrxameleonx@mail.ru>
2023-02-07 21:25:49 +0400

    task3_2
    done. Init task4

--- Lab1/lb1_3.rb
+++ Lab1/lb1_3.rb
@@ -15,6 +15,32 @@ def firstPos(arr)
 	return -1
 end
 
-a=[1,3,-4,6,8,-3,-5, 10]
-puts minArr(a)
-puts firstPos(a)
+if ARGV.count<2
+	puts 'Введите 2 аргумента(метод, путь)!!!'
+	return
+end
+
+meth = ARGV[0]
+path = ARGV[1]
+
+file = File.open(path)
+array = file.readline.split(' ').map(&:to_i)
+file.close()
+
+puts "Array: #{array}\n"
+
+case meth
+	when 'minArr'
+		puts "Min in arr: #{minArr(array)}"
+	when 'fP'
+		puts "First positive in arr: #{firstPos(array)}"
+	else
+		puts "Unknown method" 
+end
+
+
+
+
+#a=[1,3,-4,6,8,-3,-5, 10]
+#puts minArr(a)
+#puts firstPos(a)
