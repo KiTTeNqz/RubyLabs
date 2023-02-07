4571530f39491abb1ffb008416f65269d4bc5382
papaSquid <mrxameleonx@mail.ru>
2023-02-07 22:06:18 +0400

    task4_3(26)
    done. fix task4_2

--- Lab1/lb1_4.rb
+++ Lab1/lb1_4.rb
@@ -2,13 +2,22 @@ def idxMin(arr)
 	return arr.index(arr.min)
 end
 
+
 def countInterval(arr, a, b)
-	return arr.values_at(a..b).length
+	return arr.slice(a..b).length
 end
 
 
+def idxLastMin(arr)
+	return arr.rindex(arr.min)
+end
 
-a=[1,3,-4,6,8,-3,-5, 10]
+def betweenMins(arr)
+	return arr.slice(idxMin(arr)+1...idxLastMin(arr)).length
+end
+
+a=[1,3,-4,-5,6,8,-3,-5,10]
 #puts "Array: #{a}\n"
 #puts idxMin(a)
 #puts countInterval(a, 5, -1)
+#puts betweenMins(a)
