8e0ec31b94da1027cbb96b1b9559a7aa084168fa
papaSquid <mrxameleonx@mail.ru>
2023-02-07 12:21:26 +0400

    task2_3
    done

--- Lab1/lb1_2.rb
+++ Lab1/lb1_2.rb
@@ -5,13 +5,24 @@ def vzaimProst(num1, num2)
 	end
 end
 
+def formArrDel(num)
+	return 1.upto(num).select{|e| num%e==0}
+end
+
 def meth1(num1)
-	return (2.upto(num1-1).select {|e| num1.gcd(e)==1}).length
+	return 2.upto(num1-1).count{|e| num1.gcd(e)==1}
 end
 
 def meth2(num)
 	return num.digits.select{|e| e%3==0}.sum
 end
 
+def meth3(num)
+	#formirovat massiv iz kol-va vzaimno-prostih s dannim
+	arr = formArrDel(num).map{|e| num.digits.count{|e1| e.gcd(e1)==1}}
+	return formArrDel(num)[arr.rindex(arr.max)]
+end
+
 puts meth1(16)
 puts meth2(161718192023)
+puts meth3(17)
