5dd6e320ef24ae386ddf83da32c88fb3c754fd53
papaSquid <mrxameleonx@mail.ru>
2023-02-07 11:09:44 +0400

    task2_2
    done

--- Lab1/lb1_2.rb
+++ Lab1/lb1_2.rb
@@ -1,3 +1,4 @@
+#var2
 def vzaimProst(num1, num2)
 	if(num1.gcd(num2)==1) then true
 		else false
@@ -8,4 +9,9 @@ def meth1(num1)
 	return (2.upto(num1-1).select {|e| num1.gcd(e)==1}).length
 end
 
+def meth2(num)
+	return num.digits.select{|e| e%3==0}.sum
+end
+
 puts meth1(16)
+puts meth2(161718192023)
