69baed5090376735da1b809e736e6985114f5234
papaSquid <mrxameleonx@mail.ru>
2023-02-07 22:26:58 +0400

    task4_5(50)
    doneTask4

--- Lab1/lb1_4.rb
+++ Lab1/lb1_4.rb
@@ -20,9 +20,15 @@ def countInNums(arr, a, b)
 	return arr.select{|e| e.between?(a,b)}.length
 end
 
-a=[1,3,-4,-5,6,8,-3,-5,10]
+def differ2Arrs(arr1, arr2)
+	return arr1.find_all{|x| arr1.count(x)==1}.difference(arr2).union(arr2.find_all{|x| arr2.count(x)==1}.difference(arr1))
+end
+
+a=[1,3,-4,-5,6,8,-3,-5,10,3]
+b=[2,8,-4,-7,6,12,-15,-3,1,1]
 #puts "Array: #{a}\n"
 #puts idxMin(a)
 #puts countInterval(a, 5, -1)
 #puts betweenMins(a)
-puts countInNums(a, -1, 10)
+#puts countInNums(a, -1, 10)
+puts differ2Arrs(a, b)
