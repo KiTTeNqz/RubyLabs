122ccb4b6497f1204680d10356b19da52c6e8e18
papaSquid <mrxameleonx@mail.ru>
2023-02-07 22:12:46 +0400

    task4_4(38)
    Done

--- Lab1/lb1_4.rb
+++ Lab1/lb1_4.rb
@@ -16,8 +16,13 @@ def betweenMins(arr)
 	return arr.slice(idxMin(arr)+1...idxLastMin(arr)).length
 end
 
+def countInNums(arr, a, b)
+	return arr.select{|e| e.between?(a,b)}.length
+end
+
 a=[1,3,-4,-5,6,8,-3,-5,10]
 #puts "Array: #{a}\n"
 #puts idxMin(a)
 #puts countInterval(a, 5, -1)
 #puts betweenMins(a)
+puts countInNums(a, -1, 10)
