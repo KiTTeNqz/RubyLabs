1d6445dd70788a42259deef2ffdc6cfc6889b540
papaSquid <mrxameleonx@mail.ru>
2023-03-10 22:33:59 +0400

    writetxt

--- Lab2/main.rb
+++ Lab2/main.rb
@@ -12,7 +12,6 @@ def self.read_from_txt(file_path)
 	
 	all_studs = JSON.parse(stud)
 	all_studs["Students"].each do |hash_stud|
-		puts("#{hash_stud['last_name']}")
 		students << Student.new(hash_stud['last_name'], 
 								hash_stud['first_name'], 
 								hash_stud['parental_name'], 
@@ -21,6 +20,15 @@ def self.read_from_txt(file_path)
 	students
 end
 
+
+def self.write_to_txt(students,file_path)
+	out_string = '{"Students":['
+	students.each do |stud|
+		out_string+=stud.to_json+','
+	end
+	out_string = out_string.chop+"]}"
+	File.write(file_path, out_string)
+end
 
 
 stud1 = Student.new("Иванов", "Иван", "Иванович", {id: 4, phone: "+79051111111", git: "@6", telegram: "@7", email:"s0163526@edu.kubsu.ru"})
@@ -51,3 +59,12 @@ puts(stud_short.to_s())
 studs = read_from_txt('/home/dmitry/RubyLabs/Lab2/students.txt')
 
 puts(studs)
+
+short_studs = []
+studs.each do |stud|
+	short_studs << StudentShort.from_student_class(stud)
+end
+
+puts(short_studs)
+
+write_to_txt(studs, '/home/dmitry/RubyLabs/Lab2/students1.txt')
