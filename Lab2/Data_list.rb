5a9b46ea5f47401fc0a58ac1270bde328a5810d1
papaSquid <mrxameleonx@mail.ru>
2023-03-15 22:12:35 +0400

    DataList

--- Lab2/Data_list.rb
+++ Lab2/Data_list.rb
@@ -4,6 +4,17 @@ class Data_list
 		@selected_ids = []
 	end
 
-	
+	def select(number)
+		element = @list[number-1]
+		@selected_ids << element.id
+	end
+
+	def get_selected
+		raise NotImplementedError, "Должен быть реализован в наследниках"
+	end
+
+	def get_data
+		raise NotImplementedError, "Должен быть реализован в наследниках"
+	end
 
 end
