22e87e152425972eba355e067c7447fb52d5afa3
papaSquid <mrxameleonx@mail.ru>
2023-03-16 13:17:00 +0400

    pora v vuz(
    a nouta net(

--- Lab2/Student_list_txt.rb
+++ Lab2/Student_list_txt.rb
@@ -1,4 +1,12 @@
 class StudentListTxt
+
+	attr_accessor :students, :gen_id
+
+	def initialize(students)
+		self.students = students.sort_by(&:id)
+		self.gen_id = students.count + 1
+	end
+
 	def self.read_from_txt(file_path)
 		raise ArgumentError.new("File not found #{file_path}") unless File.file?(file_path)
 
@@ -18,5 +26,32 @@ class StudentListTxt
 				}.join()
 				)
 		}
+	end
+
+	def student_by_id(stud_id)
+		students.find{|s| s.id == stud_id}
+	end
+
+	def sorted
+		students.sort_by(&:fio)
+	end
+
+	def add_student(student)
+		students << student
+		student.id = gen_id
+		nextId
+	end
+
+	def replace_student(student_id, student)
+		idx = student.find{|s| s.id==student.id}
+		students[idx]=student
+	end
+
+	def delete_student(student_id)
+		students.reject! {|s| s.id==student.id}
+	end
+
+	def nextId
+		self.gen_id=students.max_by(&:id) + 1
 	end
 end
