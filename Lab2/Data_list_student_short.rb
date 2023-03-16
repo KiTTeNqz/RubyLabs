3ff6230b484000ed49f41239850f51389ad49c09
papaSquid <mrxameleonx@mail.ru>
2023-03-16 11:07:36 +0400

    pattern

--- Lab2/Data_list_student_short.rb
+++ Lab2/Data_list_student_short.rb
@@ -6,19 +6,4 @@ class DataListStudentShort < DataList
 		super(data)
 	end
 
-	def get_names
-		@list.first.instance_variables.map{|v| v.to_s[1..-1]}
-	end
-
-	def get_data
-		data = [[*(1..@list.length)]]
-		data += @list.map{|elem| instance_variables_wout_id(elem)}
-		DataTable.new(data)
-	end
-
-	private
-	def instance_variables_wout_id(object)
-		object.instance_variables.map{|v| object.instance_variable_get(v) unless v.to_sym == :@id}
-	end
-
 end
