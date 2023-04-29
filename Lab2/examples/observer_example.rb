667c8e0f53a066fe2b1a37b726e0315aa1173916
Sir Baran <71849841+KiTTeNqz@users.noreply.github.com>
2023-04-29 11:03:21 +0300

    Added observer_example.rb

--- Lab2/examples/observer_example.rb
+++ Lab2/examples/observer_example.rb
@@ -1,1 +1,51 @@
+# frozen_string_literal: true
+
+# Класс Наблюдаемый объект
+class Subject
+  attr_reader :state, :observers
+
+  def initialize(state)
+    @state = state
+    @observers = []
+  end
+
+  def add_observer(observer)
+    observers << observer
+  end
+
+  def remove_observer(observer)
+    observers.delete(observer)
+  end
+
+  def notify_observers
+    observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def update_state(state)
+    @state = state
+    notify_observers
+  end
+end
+
+# Класс Наблюдатель
+class Observer
+  def update(subject)
+    puts "Subject has updated state to: #{subject.state}"
+  end
+end
+
+# Создаем объекты Наблюдаемый объект и Наблюдатель
+subject = Subject.new("initial state")
+observer = Observer.new
+
+# Добавляем наблюдателя к наблюдаемому объекту
+subject.add_observer(observer)
+
+# Обновляем состояние наблюдаемого объекта
+subject.update_state("new state")
+
+# Результат выполнения:
+# Subject has updated state to: new state
 
